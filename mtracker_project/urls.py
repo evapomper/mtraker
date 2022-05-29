f88fe6bb4bbc031ec2a77223d2ee2e4cbcc10cdb
Eugenia <eugenia.altunina@gmail.com>
2022-05-29 14:23:49 +0300

    add swagger schema and urls

--- mtracker_project/urls.py
+++ mtracker_project/urls.py
@@ -1,12 +1,25 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.documentation import include_docs_urls
-from rest_framework.schemas import get_schema_view
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 
 API_TITLE = 'MTracker API'
 API_DESCRIPTION = 'A Web API for MTracker Project'
-schema_view = get_schema_view(title=API_TITLE)
+schema_view = get_schema_view(
+    openapi.Info(
+      title=API_TITLE,
+      default_version='v1',
+      description=API_DESCRIPTION,
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -16,5 +29,7 @@ urlpatterns = [
     path('api/v1/dj-rest-auth/', include('dj_rest_auth.urls')),
     path('api/v1/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('docs/', include_docs_urls(title=API_TITLE, description=API_DESCRIPTION)),
-    path('schema/', schema_view),
+    path('swagger.json/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
